c6de317bd530122b06bc77f76d8a1dc562e48aa1
Andrew Ecurut Angura <andre.ecurut@gmail.com>
2020-04-18 03:34:27 +0400

    Update forms.py

--- usertodo/forms.py
+++ usertodo/forms.py
@@ -1,11 +1,11 @@
-# from django.forms import ModelForm
-# from django import forms
+from django.forms import ModelForm
+from django import forms
 
-# from .models import *
+from .models import *
 
-# class TaskForm(forms.ModelForm):
-#     	title= forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new task...'}))
+class TaskForm(forms.ModelForm):
+    	title= forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new task...'}))
 
-# 	class Meta:
-# 		model = Task
-# 		fields = '__all__'
+	class Meta:
+		model = Task
+		fields = '__all__'
